2a20aa3c23be0ad49af3d825135788ee75b0e486
elysee12 <tuyisengeelysee1@gmail.com>
2026-04-26 09:53:23 +0200

    Enable CORS for all origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:8080', 'http://localhost:8081'],
+    origin: true,
     credentials: true,
   });
 
